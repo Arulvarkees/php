26dbe0d325d354452c2082c2a086c786ad27e98e
Arulvarkees <74089677+Arulvarkees@users.noreply.github.com>
2021-05-25 14:24:52 +0530

    Add files via upload

---
+++ adminlogin.php
@@ -0,0 +1,21 @@
+<html>
+<title>admin login</title>
+<center>
+ADMIN LOGIN
+<hr>
+<form method="post" action="http://localhost/banking/validate.php">
+<table>
+<tr>
+<td>Username</td><td><input type="text" name="T1"></td>
+</tr>
+<tr>
+<td>Password</td><td><input type="password" name="T2"></td>
+</tr>
+<tr>
+<td><input type="submit" name="b1" value="submit"></td>
+<td><input type="reset" name="b2" value="reset"></td>
+</tr>
+</table>
+</form>
+</center>
+</html>
